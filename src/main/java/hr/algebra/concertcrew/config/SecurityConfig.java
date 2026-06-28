1ea67f38b62586af2cefc1e5318b3f07092a0131
PavaoVedo <pavster32@gmail.com>
2026-06-28 19:14:51 +0200

    Fixed

--- src/main/java/hr/algebra/concertcrew/config/SecurityConfig.java
+++ src/main/java/hr/algebra/concertcrew/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,8 +16,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -46,6 +50,8 @@ public class SecurityConfig {
                     .requestMatchers("/api/database/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
@@ -55,7 +61,8 @@ public class SecurityConfig {
     @Order(2)
     public SecurityFilterChain mvcFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth
+                .securityMatcher(new NegatedRequestMatcher(
+                        PathPatternRequestMatcher.withDefaults().matcher("/api/**")))            .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                     "/", "/auth/**", "/css/**", "/js/**", "/webjars/**",
                     "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**",
